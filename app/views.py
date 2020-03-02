eedfb28e7303ebd055ae20142ce4879dd6eb6adc
jevoun tyrell <jevountyrell@gmail.com>
2020-03-01 20:17:40 -0500

    Commit of views code additions

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -27,6 +28,12 @@ def about():
     """Render the website's about page."""
     return render_template('about.html')
 
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    """Render the website's secure page."""
+    return render_template('secure_page.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -37,17 +44,24 @@ def login():
         if form.username.data:
             # Get the username and password values from the form.
 
+            username = form.username.data
+            password = form.password.data
+
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            user = UserProfile.query.filter_by(username=username).first()
+            
+            if user is not None and check_password_hash(user.password, password):
 
             # get user id, load into session
             login_user(user)
+            flash("You've logged in securely and successfully!!! :) ")
 
             # remember to flash a message to the user
-            return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
+            return redirect(url_for('secure_page'))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
 
 
